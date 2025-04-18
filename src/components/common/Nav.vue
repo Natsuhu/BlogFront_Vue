d3039edc9bfd8709919156689f8265e3cb640724
NatsuKaze <1127695505@qq.com>
2025-04-19 00:35:33 +0800

    增加搜索功能

--- src/components/common/Nav.vue
+++ src/components/common/Nav.vue
@@ -38,6 +38,13 @@
       <router-link to="/about" class="item" :class="{'base_mobile_hide': mobileHide,'active':$route.name==='about'}">
         ✨ 关于我
       </router-link>
+      <!--搜索框-->
+      <div class="ui search">
+        <div class="ui icon input">
+          <input class="prompt" type="text" placeholder="搜索文章内容...">
+          <i class="search icon"></i>
+        </div>
+      </div>
       <!--手机端菜单-->
       <div class="ui menu icon button base_right_top base_mobile_show" @click="toggle">
         <i class="sidebar icon"></i>
@@ -47,6 +54,8 @@
 </template>
 
 <script>
+import $ from 'jquery'
+
 export default {
   name: 'Nav',
   props: {
@@ -68,14 +77,45 @@ export default {
     //监听点击事件，收起导航菜单
     document.addEventListener('click', (e) => {
       //遍历冒泡
-      let flag = e.path.some(item => {
-        if (item === this.$refs.menu) return true
-      })
+      // let flag = e.path.some(item => {
+      //   if (item === this.$refs.menu) return true
+      // })
+      let flag = this.$refs.menu.contains(e.target)
       //如果导航栏是打开状态，且点击的元素不是Nav的子元素，则收起菜单
       if (!this.mobileHide && !flag) {
         this.mobileHide = true
       }
-    })
+    });
+    //搜索框
+    $('.ui.search').search({
+      type: 'category',
+      apiSettings: {
+        onResponse: function (searchResult) {
+          const response = {
+            results: {}
+          };
+          $.each(searchResult.data, function (index, item) {
+            const categoryName = item.categoryName, maxResults = 8;
+            if (index >= maxResults) {
+              return false;
+            }
+            if (response.results[categoryName] === undefined) {
+              response.results[categoryName] = {
+                name: categoryName,
+                results: []
+              };
+            }
+            response.results[categoryName].results.push({
+              title: item.title,
+              description: item.content,
+              url: '/#/articles/read/' + item.id
+            });
+          });
+          return response;
+        },
+        url: window.env.API_BASE_URL + '/articles/search/{query}'
+      }
+    });
   },
   methods: {
     categoryRoute(id) {
@@ -94,14 +134,12 @@ export default {
   margin-left: auto !important;
   margin-right: auto !important;
 }
-
 .el-dropdown-link {
   outline-style: none !important;
   outline-color: unset !important;
   height: 100%;
   cursor: pointer;
 }
-
 /*.el-dropdown-menu {*/
 /*	margin: 7px 0 0 0 !important;*/
 /*	padding: 0 !important;*/
@@ -117,15 +155,21 @@ export default {
 /*.el-dropdown-menu__item:hover {*/
 /*	background: rgba(255, 255, 255, .08) !important;*/
 /*}*/
-
 .ui.menu {
   background-color: rgba(255, 255, 255, 0.7);
   -webkit-backdrop-filter: blur(20px);
   backdrop-filter: blur(20px);
 }
-
 .ui.button {
   background-color: rgba(255, 255, 255, 0.0) !important;
   font-size: 1.3rem !important;
+}
+.ui.search {
+  margin-left: auto;
+  align-self: center;
+}
+/*覆盖原有样式避免超出屏幕*/
+::v-deep .ui.search>.results {
+  left: -100px;
 }
 </style>
